807dc83bc70f9e09710262452cac2ed3492ad59e
Lukas Keller <keller.lukas@bluewin.ch>
2024-08-11 20:06:57 +0200

    fix syntax

--- src/llm/smart_recipe_client.py
+++ src/llm/smart_recipe_client.py
@@ -32,7 +32,7 @@ class MockRecipeClient(RecipeClient):
     def get_recipe_details(self, recipe_id) -> dict:
         time.sleep(2)
         res = json.loads(demo_recipe)
-        res['title'] = recipe_id
+        res['title'] = str(recipe_id)
         return res
 
         
